e3e59bdbae5dd1611a2dc01b6e73fef8dd14e846
dbsasser <danielbsasser@gmail.com>
2020-06-06 15:35:13 -0400

    Added methods for encoding and decoding jwt

--- diggit-backend/app/controllers/application_controller.rb
+++ diggit-backend/app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::API
+    def encode_token(payload)
+        JWT.encode(payload, ENV['diggit_jwt_key'])
+      end
+    
+      def auth_header
+        request.headers['Authorization']
+      end
+    
+      def decoded_token
+        if auth_header
+          token = auth_header.split(' ')[1]
+          begin
+            JWT.decode(token, ENV['diggit_jwt_key'], true, algorithm: 'HS256')
+          rescue JWT::DecodeError
+            nil
+          end
+        end
+      end
 end
