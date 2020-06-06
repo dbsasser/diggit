7ab14e1f6654e36c5f7af8a776f29464927a6fbf
dbsasser <danielbsasser@gmail.com>
2020-06-06 19:01:32 -0400

    set token on create

--- diggit-backend/app/controllers/api/v1/users_controller.rb
+++ diggit-backend/app/controllers/api/v1/users_controller.rb
@@ -2,14 +2,15 @@ class Api::V1::UsersController < ApplicationController
     def create
         @user = User.create(user_params)
         if @user.valid?
-          render json: @user, status: :created
+            @token = encode_token(user_id: @user.id)
+            render json: { user: @user, jwt: @token } status: :created
         else
-          render json: { error: 'failed to create user' }, status: :not_acceptable
+            render json: { error: 'failed to create user' }, status: :not_acceptable
         end
       end
     
-      private
-      def user_params
-        params.require(:user).permit(:username, :password)
-      end
+    private
+        def user_params
+            params.require(:user).permit(:username, :password)
+        end
 end
