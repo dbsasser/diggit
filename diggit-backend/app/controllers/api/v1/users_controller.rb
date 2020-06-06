a15bf5de6220bf311fade29f3762e5ee0fa61ac3
dbsasser <danielbsasser@gmail.com>
2020-06-06 11:54:13 -0400

    Fixed syntax error in create method

--- diggit-backend/app/controllers/api/v1/users_controller.rb
+++ diggit-backend/app/controllers/api/v1/users_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::UsersController < ApplicationController
     def create
         @user = User.create(user_params)
         if @user.valid?
-          render json: { @user }, status: :created
+          render json: @user, status: :created
         else
           render json: { error: 'failed to create user' }, status: :not_acceptable
         end
