7e8200626df4fda1dad33ec59b7a3268f8bc1911
dbsasser <danielbsasser@gmail.com>
2020-06-07 08:59:08 -0400

    Added login form in modal

--- diggit-frontend/src/index.js
+++ diggit-frontend/src/index.js
@@ -11,7 +11,15 @@ function userModal () {
     signup.addEventListener('click', function(event) {
         event.preventDefault();
         modal.style.display = "block";
-        modalContent.innerHTML = "Form Placeholder"
+        modalContent.innerHTML = `
+        <form id="user-form">
+        <label for="username">Username</label>
+        <input type="text" name="username" id="username">
+        <label for="password">Password</label>
+        <input type="password" name="password" id="password">
+        <input type="submit" value="Log In">
+        </form>
+        `
 
         modal.addEventListener('click', function(event){
             if (event.target === modal) {
