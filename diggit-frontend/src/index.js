6fc4f7d7ac88c6377e60d931a73013d08247a816
dbsasser <danielbsasser@gmail.com>
2020-06-06 21:10:45 -0400

    added event to display modal on signup click

--- diggit-frontend/src/index.js
+++ diggit-frontend/src/index.js
@@ -7,5 +7,10 @@ function userModal () {
     const login = document.getElementById("login");
     const modal = document.getElementById("modal");
     const modalContent = document.getElementById("modal-content");
-    }
+
+    signup.addEventListener('click', function(event) {
+        event.preventDefault();
+        modal.style.display = "block";
+        modalContent.innerHTML = "Form Placeholder"
+    })
 }
