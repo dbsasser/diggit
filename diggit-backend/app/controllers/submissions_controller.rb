83eed8a49ff82b4fe3e32b21e3d49ecd27ac1f8d
dbsasser <danielbsasser@gmail.com>
2020-06-07 19:53:54 -0400

    Added create and index methods for submissions

--- diggit-backend/app/controllers/submissions_controller.rb
+++ diggit-backend/app/controllers/submissions_controller.rb
@@ -1,2 +1,25 @@
 class SubmissionsController < ApplicationController
+
+    def create 
+        @submission = Submission.new(submission_params)
+        if @submission.save
+            render json: { submission: @submission_params }, status: :created
+        else 
+            render json: { error: 'failed to create post' }, status: :not_acceptable
+        end
+    end
+
+    def index
+        submissions = Submission.all 
+        render json: submissions
+    end
+
+    private
+
+    def submission_params 
+        params.require(:submission).permit(:link, :category_id, :user_id)
+    end
+
 end
+
+
