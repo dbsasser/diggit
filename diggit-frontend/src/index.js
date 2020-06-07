7ec0fb0f091555cddd006a237c172414e4f71c6c
dbsasser <danielbsasser@gmail.com>
2020-06-06 20:24:33 -0400

    Added userModal function

---
+++ diggit-frontend/src/index.js
@@ -0,0 +1,11 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    userModal()
+});
+
+function userModal () {
+    const signup = document.getElementById("signup");
+    const login = document.getElementById("login");
+    const modal = document.getElementById("modal");
+    const modalContent = document.getElementById("modal-content");
+    }
+}
