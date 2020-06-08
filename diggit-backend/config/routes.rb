b3ba70abb403c2b0a506a3d3430b79b1a1a4027c
dbsasser <danielbsasser@gmail.com>
2020-06-07 20:15:38 -0400

    moved cat and sub resources into v1 namespace

--- diggit-backend/config/routes.rb
+++ diggit-backend/config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  resources :categories
-  resources :submissions
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
       resources :users, only: [:create]
       post '/login', to: 'auth#create'
       get '/profile', to: 'users#profile'
+      resources :categories
+      resources :submissions
     end
   end
 end
