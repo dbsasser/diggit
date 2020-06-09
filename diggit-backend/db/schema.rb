729afc3b3e55810167741a19aec164bbe0655276
dbsasser <danielbsasser@gmail.com>
2020-06-09 15:24:58 -0400

    Migrated db

--- diggit-backend/db/schema.rb
+++ diggit-backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_09_150939) do
+ActiveRecord::Schema.define(version: 2020_06_09_192139) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,15 @@ ActiveRecord::Schema.define(version: 2020_06_09_150939) do
     t.index ["user_id"], name: "index_submissions_on_user_id"
   end
 
+  create_table "upvotes", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "submission_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["submission_id"], name: "index_upvotes_on_submission_id"
+    t.index ["user_id"], name: "index_upvotes_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password_digest"
@@ -41,4 +50,6 @@ ActiveRecord::Schema.define(version: 2020_06_09_150939) do
 
   add_foreign_key "submissions", "categories"
   add_foreign_key "submissions", "users"
+  add_foreign_key "upvotes", "submissions"
+  add_foreign_key "upvotes", "users"
 end
