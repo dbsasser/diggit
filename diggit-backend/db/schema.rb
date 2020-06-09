9b8b06923e7f0de8a8db1a084cc0c4866b9a57e5
dbsasser <danielbsasser@gmail.com>
2020-06-09 11:10:53 -0400

    Add title column to Submissions table

--- diggit-backend/db/schema.rb
+++ diggit-backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_07_230807) do
+ActiveRecord::Schema.define(version: 2020_06_09_150939) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2020_06_07_230807) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "title"
     t.index ["category_id"], name: "index_submissions_on_category_id"
     t.index ["user_id"], name: "index_submissions_on_user_id"
   end
