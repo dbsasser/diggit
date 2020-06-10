2db157da739fc16626e1ad16341e581eebc82d15
dbsasser <danielbsasser@gmail.com>
2020-06-09 21:43:00 -0400

    Added create method for upvote

--- diggit-backend/app/controllers/api/v1/upvotes_controller.rb
+++ diggit-backend/app/controllers/api/v1/upvotes_controller.rb
@@ -1,2 +1,17 @@
-class UpvotesController < ApplicationController
+class Api::V1::UpvotesController < ApplicationController
+    def create 
+        @upvote = Upvote.new(upvote_params)
+        @upvote.user_id = current_user.id
+        if @upvote.save
+            render json: { upvote: @upvote }, status: :created
+        else 
+            render json: { error: 'failed to upvote' }, status: :not_acceptable
+        end
+    end
+
+    private
+
+    def upvote_params
+        params.require(:submission).permit(:submission_id, :user_id)
+    end
 end
