6ecb07ac2270a3a84773b7f65dfdbff7a8aa5991
dbsasser <danielbsasser@gmail.com>
2020-06-18 12:36:52 -0400

    added create resource for submissions

--- diggit-backend/config/routes.rb
+++ diggit-backend/config/routes.rb
@@ -2,13 +2,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :users, only: [:create]
+      resources :users, only: [:create, :show]
       post '/login', to: 'auth#create'
       get '/profile', to: 'users#profile'
       resources :categories do 
         resources :submissions, only: [:index, :show]
       end
-      resources :submissions, only: [:index, :show]
+      resources :submissions, only: [:index, :show, :create]
       resources :upvotes, only: [:create]
     end
   end
